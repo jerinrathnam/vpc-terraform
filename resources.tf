1b88cd74164665a2b3e13223b456ff5cd18f8894
jerinrathnam <jerinrathnam@easydeploy.cloud>
2022-06-22 16:25:38 +0530

    first commit

---
+++ resources.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key 
+  #profile = var.profile
+  region  = var.region
+}
+
+
+data "aws_availability_zones" "available" {}
+
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~>3.14.1"
+
+  name = "jerin-primary"
+
+  cidr            = var.cidr_block
+  azs             = slice(data.aws_availability_zones.available.names, 0, var.subnet_count)
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = false
+
+  create_database_subnet_group = false
+
+
+  tags = {
+    Environment = "Production"
+    Team        = "Network"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "profile" {
+  type = string
+  
+}
+variable "subnet_count" {
+  default = 2
+}
+
+variable "cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "private_subnets" {
+  type = list(any)
+}
+
+variable "public_subnets" {
+  type = list(any)
+} 
+
+variable "aws_access_key" {
+  type      = string
+  sensitive = true
+}
+variable "aws_secret_key" {
+  type      = string
+  sensitive = true
+}
