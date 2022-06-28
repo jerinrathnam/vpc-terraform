77e4575945684b764fa3f15533d494c3e8ecb9c6
JerinRathnam <61724686+jerinrathnam@users.noreply.github.com>
2022-06-28 14:14:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,15 +7,15 @@ variable "subnet_count" {
 }
 
 variable "cidr_block" {
-  default = "10.0.0.0/16"
+  default = "10.0.0.0/24"
 }
 
 variable "public_cidr_block" {
-  default = "10.1.0.0/16"
+  default = "10.0.1.0/24"
 }
 
 variable "private_cidr_block" {
-  default = "10.2.0.0/16"
+  default = "10.0.2.0/24"
 }
 /*
 variable "private_subnets" {
