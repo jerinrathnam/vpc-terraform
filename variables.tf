95d9c30f21615f77c9d38c927186bc67b9e1f4ea
JerinRathnam <61724686+jerinrathnam@users.noreply.github.com>
2022-07-04 09:59:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,22 +9,7 @@ variable "subnet_count" {
 variable "cidr_block" {
   default = "10.0.0.0/24"
 }
-
-variable "public_cidr_block" {
-  default = "10.0.1.0/24"
-}
-
-variable "private_cidr_block" {
-  default = "10.0.2.0/24"
-}
-/*
-variable "private_subnets" {
-  type = list(any)
-}
-
-variable "public_subnets" {
-  type = list(any)
-} 
+/* 
 variable "profile" {
   type = string
   
